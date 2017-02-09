cb50221244c03ca858e175d568733d5c0d27fdf0
klauBertram <klau@bcap.com>
2017-02-09 14:57:14 -0800

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -35,6 +35,21 @@ io.on('connection', (socket) => {
   //   createdAt: new Date()
   // });
 
+  // socket.emit from admin, text - welcome to the chat app
+  socket.emit('newMessage', {
+    from: 'ADMIN',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  // socket.broadcast.emit from admin, text - new user joined
+  // all new message event
+  socket.broadcast.emit('newMessage', {
+    from: 'ADMIN',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('create message', message);
 
@@ -45,6 +60,13 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // broadcasting - emit to everyone except 1 user
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
 
     // socket.emit('newMessage', {
     //   from: message.from + ' (returned)',
