8db91645c8e8ff796515354777383a43d89dcf49
klauBertram <klau@bcap.com>
2017-02-24 16:52:45 -0800

    add mustache js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -21,26 +21,46 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message){
-  // console.log('new message', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = $('#message-template').html();
+  var html = Mustache.render(template, {
+    createdAt: formattedTime,
+    from: message.from,
+    text: message.text
+  });
 
-  var li = $('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  $('#messages').append(html);
 
-  $('#messages').append(li);
+  // console.log('new message', message);
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+
+  // var li = $('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+  // $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
-  console.log('getting newLocationMessage', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  var a = $('<a target="_blank">My current location</a>');
+  var template = $('#location-message-template').html();
+  var html = Mustache.render(template, {
+    createdAt: formattedTime,
+    from: message.from,
+    url: message.url
+  });
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
+  $('#messages').append(html);
 
-  $('#messages').append(li);
+  // console.log('getting newLocationMessage', message);
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+  // var li = $('<li></li>');
+  // var a = $('<a target="_blank">My current location</a>');
+
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+
+  // $('#messages').append(li);
 });
 
 // socket.on('newEmail', function(email){
