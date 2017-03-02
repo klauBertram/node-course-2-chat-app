87f59faa8b4be52345ce7a25a52176d82fb608a6
Kenn K. Lau <klau@bertramcapital.com>
2017-03-01 23:54:13 -0800

    got deparam lib in repo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const moment = require('moment');
 const path = require('path');
 const app = express();
 const server = http.createServer(app);
